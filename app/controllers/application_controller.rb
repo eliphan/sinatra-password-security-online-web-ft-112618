2c79f204f8be1681dbf13fb0df319313e5774cac
Elise Phan <aliceconnic@gmail.com>
2019-01-25 06:10:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,14 +33,12 @@ class ApplicationController < Sinatra::Base
 	post "/login" do
     user = User.find_by(:username => params[:username])
     
-    
-    
-    if user
-      redirect "/success"
+     if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/success"
      else
-      redirect "/failure"
-     end
-	end
+        redirect "/failure"
+  end 
 
 	get "/success" do
 		if logged_in?
